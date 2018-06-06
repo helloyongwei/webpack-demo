210a7b1749f10d87df61f80ff9a4f93895b13698
Helloyong <2628578405@qq.com>
2018-06-06 13:48:49 +0800

    add full files

---
+++ src/index.js
@@ -0,0 +1,17 @@
+ import _ from 'lodash';
+ import './style.css'
+ import './main.scss'
+ 
+
+  function component() {
+    var element = document.createElement('div');
+
+   // Lodash, currently included via a script, is required for this line to work
+   // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('')
+
+    return element;
+  }
+
+  document.body.appendChild(component());
